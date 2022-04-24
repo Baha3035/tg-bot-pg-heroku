ea0420c064def47bc14ab03044be83f11567a771
Baha <batmaaanpls@gmail.com>
2022-04-24 16:27:38 +0600

    update db uri

--- data/config.py
+++ data/config.py
@@ -5,6 +5,6 @@ PROJECT_NAME = 'store-bot-example'
 WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
 WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-DB_URI = "postgres://xmgdswtykrnjpa:6e3bade32ee29a658bca98af21a8916896e5c33b383c4048153107fbec8365d2@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/dfqijgrnpc42tk"
+DB_URI = "postgres://evhdxwaqxcowbu:35f37f26c91c015af5e4d3eb0d5ec326e1597f178336bffb0755d370680e2d25@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/detshm2lk759f8"
 
 ADMINS = [000000000, 1234567890]
