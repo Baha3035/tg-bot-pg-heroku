43fd47f15def2ca99ccadcbf147b18ce280bff39
Baha <batmaaanpls@gmail.com>
2022-04-27 17:55:57 +0600

    new db credentials

--- data/config.py
+++ data/config.py
@@ -5,6 +5,6 @@ PROJECT_NAME = 'tg-bot-delivery'
 WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
 WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-DB_URI = "postgres://evhdxwaqxcowbu:35f37f26c91c015af5e4d3eb0d5ec326e1597f178336bffb0755d370680e2d25@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/detshm2lk759f8"
+DB_URI = "postgres://yeltmzlfdttraj:09503e6d76ea6183612ed1e57e1373d1e23c4c31ac5459910dd9bd464958a02b@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/dgbgiq2gihjue"
 
 ADMINS = [000000000, 1234567890]
