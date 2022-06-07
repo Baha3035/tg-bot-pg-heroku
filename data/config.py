ea789d936c5fd2251d4f64503630329843a5ccaf
Baha <batmaaanpls@gmail.com>
2022-06-07 13:07:10 +0600

    karakol-bot

--- data/config.py
+++ data/config.py
@@ -1,10 +1,10 @@
-BOT_TOKEN = '5349137146:AAEhEOYgN0BZYq9Pao2otgGHBiejz9WrUgc'
+BOT_TOKEN = '5543275879:AAGX-OgnFZntckTy5QgsA1K0516GKtFOjsM'
 
-PROJECT_NAME = 'tg-bot-delivery'
+PROJECT_NAME = 'tg-bot-karakol'
 
 WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
 WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-DB_URI = "postgres://yeltmzlfdttraj:09503e6d76ea6183612ed1e57e1373d1e23c4c31ac5459910dd9bd464958a02b@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/dgbgiq2gihjue"
+DB_URI = "postgres://ektogflsrfihxo:93ddb02e8faa0cb1f597e0cdc3d1c110b1556c8e888a492ee69e815a119f141e@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/d60pj0v7mt71vb"
 
 ADMINS = [000000000, 1234567890]
